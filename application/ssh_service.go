14b4f24de57b861ce8d8bea46ed00419f085b1dc
Mikel Bravo <me@mklbravo.net>
2025-06-26 11:36:47 +0200

    refactor(app): update main and application layer for new domain and infrastructure structure

--- application/ssh_service.go
+++ application/ssh_service.go
@@ -1,12 +1,16 @@
 package application
 
 import (
-	"github.com/mklbravo/sshp/domain"
-	"github.com/mklbravo/sshp/infrastructure"
+    "github.com/mklbravo/sshp/domain"
+    "github.com/mklbravo/sshp/infrastructure"
 )
 
-func ConnectToSSHHost(host *domain.Host) {
-	sshSession, _ := infrastructure.StartSSHSession(host)
-	infrastructure.RunSSHShell(sshSession)
-	defer sshSession.Close()
+func ConnectToSSHHost(host domain.Host) error {
+    sshSession, err := infrastructure.StartSSHSession(&host)
+    if err != nil {
+        return err
+    }
+    defer sshSession.Close()
+    infrastructure.RunSSHShell(sshSession)
+    return nil
 }
