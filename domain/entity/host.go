400c19570704f079abf78b61f0b3063e910d9a76
Mikel Bravo <me@mklbravo.net>
2025-09-16 11:31:46 +0000

    feat: add constructor func

--- domain/entity/host.go
+++ domain/entity/host.go
@@ -14,6 +14,30 @@ type Host struct {
 	Port     valueobject.Port
 }
 
+func NewHost(id int, name, username, ip string, port int) (*Host, error) {
+	hostName := valueobject.HostName(name)
+
+	user := valueobject.Username(username)
+
+	hostIP, err := valueobject.NewIP(ip)
+	if err != nil {
+		return nil, fmt.Errorf("invalid IP address: %w", err)
+	}
+
+	hostPort, err := valueobject.NewPort(port)
+	if err != nil {
+		return nil, fmt.Errorf("invalid port: %w", err)
+	}
+
+	return &Host{
+		ID:       id,
+		Name:     hostName,
+		Username: user,
+		IP:       hostIP,
+		Port:     hostPort,
+	}, nil
+}
+
 func (this *Host) GetFullAddress() string {
 	return fmt.Sprintf("%s:%d",
 		string(this.IP),
