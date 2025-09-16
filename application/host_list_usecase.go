133fd2277a79df7817b3fc70155f5b7609a5f08c
Mikel Bravo <me@mklbravo.net>
2025-09-16 11:37:12 +0000

    feat: add HostListUseCase

---
+++ application/host_list_usecase.go
@@ -0,0 +1,25 @@
+package application
+
+import (
+	"github.com/mklbravo/sshp/domain/entity"
+	"github.com/mklbravo/sshp/domain/repository"
+)
+
+type HostListUseCase struct {
+	HostRepository repository.HostRepository
+}
+
+func NewHostListUseCase(hostRepo repository.HostRepository) *HostListUseCase {
+	return &HostListUseCase{
+		HostRepository: hostRepo,
+	}
+}
+
+func (uc *HostListUseCase) Execute() ([]*entity.Host, error) {
+	hostList, err := uc.HostRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return hostList, nil
+}
