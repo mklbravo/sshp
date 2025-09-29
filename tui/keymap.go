1c8cc0a94e28f0a344a005feb0a1f5b73811a1c6
Mikel Bravo <me@mklbravo.net>
2025-09-29 11:46:03 +0000

    feat(tui): define down/up keys

--- tui/keymap.go
+++ tui/keymap.go
@@ -3,11 +3,19 @@ package tui
 import "github.com/charmbracelet/bubbles/key"
 
 type keyMapStruct struct {
+	Down key.Binding
 	Quit key.Binding
+	Up   key.Binding
 }
 
 var keyMap = keyMapStruct{
+	Down: key.NewBinding(
+		key.WithKeys("down"),
+	),
 	Quit: key.NewBinding(
 		key.WithKeys("ctrl+c", "esc"),
+	),
+	Up: key.NewBinding(
+		key.WithKeys("up"),
 	),
 }
