efbcb5e536a7cf61aeaf1522d892a5c9eee1885f
Mikel Bravo <me@mklbravo.net>
2025-10-06 22:28:00 +0000

    feat: add json/host_repository

---
+++ infrastructure/json/host_repository.go
@@ -0,0 +1,64 @@
+package json
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/mklbravo/sshp/domain/entity"
+)
+
+type JsonHostRepository struct {
+	allHosts     []*entity.Host
+	indexedHosts map[int]*entity.Host
+}
+
+func NewJsonHostRepository(filePath string) (*JsonHostRepository, error) {
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var hostsData []JsonHostData
+	err = json.Unmarshal(fileData, &hostsData)
+	if err != nil {
+		return nil, err
+	}
+	var allHosts []*entity.Host
+	var indexedHosts = make(map[int]*entity.Host)
+
+	for _, hd := range hostsData {
+		host, err := entity.NewHost(hd.ID, hd.Name, hd.User, hd.Address, hd.Port)
+		if err != nil {
+			return nil, err
+		}
+		allHosts = append(allHosts, host)
+		indexedHosts[host.ID] = host
+	}
+
+	return &JsonHostRepository{
+		allHosts:     allHosts,
+		indexedHosts: indexedHosts,
+	}, nil
+
+}
+
+func (this *JsonHostRepository) FindByID(id string) (*entity.Host, error) {
+	// TODO
+	return nil, nil
+}
+func (this *JsonHostRepository) FindAll() ([]*entity.Host, error) {
+	return this.allHosts, nil
+}
+
+func (this *JsonHostRepository) Save(host *entity.Host) error {
+	// TODO
+	return nil
+}
+
+type JsonHostData struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	User    string `json:"user"`
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+}
