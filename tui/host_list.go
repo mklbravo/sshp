e5e7692bed82780af7aca4c20f291439603ee3ca
Mikel Bravo <me@mklbravo.net>
2025-10-20 11:29:48 +0000

    feat: render details on view

--- tui/host_list.go
+++ tui/host_list.go
@@ -131,6 +131,8 @@ func (this Model) View() string {
 			string(host.Username),
 			colorStyle.sky.Render(" "),
 			string(host.IP),
+			colorStyle.sky.Render(" "),
+			host.GetDetailsString(),
 		)
 	}
 
