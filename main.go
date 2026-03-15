172223c1a24a76f3ea53fca57a03ff9aee4a1c80
Mikel Bravo <me@mklbravo.net>
2026-03-15 00:03:51 +0000

    refactor(main): migrate entry point to cobra CLI structure
    Refactored the main application entry point to use the Cobra library for command-line interface management. Moved the core logic into a Cobra root command for improved structure and extensibility.

--- main.go
+++ main.go
@@ -10,46 +10,61 @@ import (
 	"github.com/mklbravo/sshp/infrastructure/ssh"
 	"github.com/mklbravo/sshp/internal/config"
 	"github.com/mklbravo/sshp/tui"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	hostRepository, err := json.NewJsonHostRepository(cfg.DataFilePath)
-	if err != nil {
-		log.Fatalf("Failed to load hosts: %v", err)
+	rootCmd := createRootCommand()
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Error executing command: %v", err)
 		os.Exit(1)
 	}
+}
 
-	hostListUC := application.NewHostListUseCase(hostRepository)
+func createRootCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "sshp",
+		Short: "SSHP is a terminal-based SSH host manager and connector.",
+		Run: func(cmd *cobra.Command, args []string) {
+			cfg, err := config.Load()
+			if err != nil {
+				log.Fatalf("Failed to load config: %v", err)
+			}
 
-	hostListView := tui.NewHostListView(hostListUC)
+			hostRepository, err := json.NewJsonHostRepository(cfg.DataFilePath)
+			if err != nil {
+				log.Fatalf("Failed to load hosts: %v", err)
+				os.Exit(1)
+			}
 
-	tuiProgram := tea.NewProgram(hostListView)
+			hostListUC := application.NewHostListUseCase(hostRepository)
 
-	teaModel, err := tuiProgram.Run()
+			hostListView := tui.NewHostListView(hostListUC)
 
-	if err != nil {
-		log.Fatalf("Error running program: %v", err)
-		os.Exit(1)
-	}
-	model := teaModel.(tui.Model)
+			tuiProgram := tea.NewProgram(hostListView)
 
-	selectedHost := model.GetSelectedHost()
-	if selectedHost == nil {
-		os.Exit(0)
-	}
+			teaModel, err := tuiProgram.Run()
 
-	hostConnectionUC := application.NewHostConnectionUseCase(
-		ssh.NewSSHConnectionService(),
-	)
+			if err != nil {
+				log.Fatalf("Error running program: %v", err)
+				os.Exit(1)
+			}
+			model := teaModel.(tui.Model)
 
-	err = hostConnectionUC.Execute(selectedHost)
-	if err != nil {
-		log.Fatalf("Failed to connect to SSH host: %v", err)
-		os.Exit(1)
+			selectedHost := model.GetSelectedHost()
+			if selectedHost == nil {
+				os.Exit(0)
+			}
+
+			hostConnectionUC := application.NewHostConnectionUseCase(
+				ssh.NewSSHConnectionService(),
+			)
+
+			err = hostConnectionUC.Execute(selectedHost)
+			if err != nil {
+				log.Fatalf("Failed to connect to SSH host: %v", err)
+				os.Exit(1)
+			}
+		},
 	}
 }
