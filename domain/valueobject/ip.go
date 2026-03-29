0af5221f9f30e9843e63d9dfe55aec9fb807da18
Mikel Bravo <me@mklbravo.net>
2026-03-29 21:38:27 +0000

    style(domain/valueobject): use fmt.Errorf instead of errors.New in ip.go

--- domain/valueobject/ip.go
+++ domain/valueobject/ip.go
@@ -1,7 +1,6 @@
 package valueobject
 
 import (
-	"errors"
 	"fmt"
 	"net"
 )
@@ -10,7 +9,7 @@ type IP string
 
 func NewIP(ip string) (IP, error) {
 	if net.ParseIP(ip) == nil {
-		err := errors.New(fmt.Sprintf("Invalid IP address format: %s", ip))
+		err := fmt.Errorf("Invalid IP address format: %s", ip)
 		return "", err
 	}
 	return IP(ip), nil
